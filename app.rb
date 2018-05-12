f87ffb01a2e0fd914413672a4f325e4b19514e27
Jen Poole <contact.vandepool@gmail.com>
2018-05-12 03:06:54 +0000

    render instance variable from controller in ERB template

--- app.rb
+++ app.rb
@@ -5,7 +5,14 @@ class App < Sinatra::Base
     erb :reverse
   end
 
+  # manipulate the string by getting it from the params hash and calling the .reverse method on it
+  # Instance variables allow us to bypass scope between the various methods in a class
+  # Creating an instance variable in a controller method (route) lets the contents become 'visible' to the erb file to which it renders
+  # Instead of creating a local variable reversed_string, change it to an instance variable @reversed_string
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+
     erb :reversed
   end
 
